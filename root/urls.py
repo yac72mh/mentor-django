589791987a01facf3116558197379a9943cf26a7
yac72mh <mehrabi.yac@gmail.com>
2023-08-18 10:11:28 -0700

    tem

--- root/urls.py
+++ root/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home , about , contact
 
 urlpatterns = [
-    path("" , home)
-     
+    path("" , home),
+    path("about" , about),
+    path("contact" , contact), 
 ]
--- root/views.py
+++ root/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("heloo django")
+    return render (request , "index.html")
 
-# Create your views here.
+def about(request):
+    return render (request , "about.html")
+
+def contact(request):
+    return render (request , "contact.html")
+
+
