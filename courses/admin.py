6636761604a728f9b6df927c5b18316fb068f7f8
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 12:30:27 -0700

    model

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Courses
+from .models import *
 
 admin.site.register(Courses)
+admin.site.register(Skills)
+admin.site.register(Trainer)
+admin.site.register(Category)
 
 # Register your models here.
+
