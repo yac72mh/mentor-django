a28575b9ed533593ef992d59e64b4eb9b07b98ee
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 09:20:02 -0700

    cours

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def courses(request):
-    return render (request , 'course/courses.html')
+    return render (request , 'courses/courses.html')
 
 # Create your views here.
