02ec84a161868b3ab3780f2d3a523a4d1d9e0caf
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 13:11:56 -0700

    123

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'root',
-    'courses'
+    'courses',
 ]
 
 MIDDLEWARE = [
--- root/views.py
+++ root/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Services
+from courses.models import Courses , Trainer
 
 def home(request):
     service = Services.objects.filter(status=True)
+    last_three_courses = Courses.objects.filter(status=True)[:3]
+    last_three_Trainer = Trainer.objects.filter(status=True)[:3]
     context = {
         'service': service,
+        'courses' : last_three_courses,
+        'trainer': last_three_Trainer,
    }
     return render (request , "root/index.html", context= context)
 
