0a17cf714dfe74a71be9db4092b9fb87a0a2f91f
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 07:07:46 -0700

    ser

--- root/views.py
+++ root/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Services
 
 def home(request):
-    return render (request , "root/index.html")
+    services = Services.objects.all()
+    context = [
+        'services': Services
+    ]
+    return render (request , "root/index.html", context= context)
 
 def about(request):
     return render (request , "root/about.html")
