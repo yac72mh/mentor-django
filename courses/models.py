f9c550b48392ac7d2b5c86cc55228fd1b5a313de
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 09:28:14 -0700

    cour

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Courses(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    price = models.IntegerField(default=0)
+
+
+def __str__(self):
+    return self.title
