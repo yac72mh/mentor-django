6b7ec73d1cdf3370889265734cf9b4da31852302
yac72mh <mehrabi.yac@gmail.com>
2023-08-26 07:33:42 -0700

    aaa

--- root/views.py
+++ root/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Services
 
 def home(request):
-    services = Services.objects.all()
+    services = Services.objects.filter(status=True)
     context = {
         'services': Services,
    }
